eb9aa8360fe2466fd4abead4a8ee2de94f872dac
Nathan Raposo <nathanraposoolivo@gmail.com>
2022-04-21 17:52:52 -0300

    Understanding routing in vue and how to style components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
+// import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App);
+
+app.component('teste',{
+    template:'<h2>Teste</h2>'
+})
 
 app.use(router)
 
