55226db7c4b6df7c01938cf66b9b35b30bc7af11
Nathan Raposo <nathanraposoolivo@gmail.com>
2022-04-21 16:47:47 -0300

    add readme

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(router)
 
