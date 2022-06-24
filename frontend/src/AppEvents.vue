fa554fc9f76d30e7901df67aa958be53730f85c9
Nathan Raposo <nathanraposoolivo@gmail.com>
2022-06-23 22:26:42 -0300

    form dinamico

---
+++ frontend/src/AppEvents.vue
@@ -0,0 +1,51 @@
+<template>
+  <img
+      :src="imageSrc"
+      class="my-default-class"
+      :class="{'my-class':is_admin,'my-other-class':!is_admin}">
+
+  <button @dblclick="add">Clique aqui {{ count }}</button>
+  <h2>Lista de Usuários</h2>
+  <ul>
+    <li v-for="user in users.users" :key="user.id">
+      {{ user.firstName }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import {onMounted, reactive, ref} from "vue";
+import http from '@/services/http.js'
+
+let users = reactive({
+  users: []
+})
+
+const imageSrc = ref('https://picsum.photos/200/300')
+let is_admin = ref(true)
+const count = ref(0)
+
+onMounted(async () => {
+  try {
+    const {data} = await http.get('/api/users')
+    users.users = data
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+function add() {
+  count.value++
+}
+
+</script>
+
+<style scoped>
+.my-class {
+  border: solid 4px red;
+}
+
+.my-other-class {
+  border: solid 4px blue;
+}
+</style>
---
+++ frontend/src/AppFormDinamico.vue
@@ -0,0 +1,41 @@
+<template>
+  <input type="text" v-model="userName">
+
+  <template v-for="user in users">
+    <input type="checkbox"
+           @click="userName = user.firstName"
+           :checked="userName === user.firstName">
+           {{ user.firstName }}
+  </template>
+
+  <select v-model="userName">
+    <option :value="user.firstName" v-for="user in users">{{ user.firstName }}</option>
+  </select>
+  <text></text>
+</template>
+
+<script setup>
+
+import {reactive, ref} from "vue";
+
+const userName = ref('Nathan')
+const users = reactive([
+      {
+        id: 1,
+        firstName: 'Nathan'
+      },
+      {
+        id: 2,
+        firstName: 'Natalia'
+      },
+      {
+        id: 1,
+        firstName: 'Micaella'
+      }
+    ]
+)
+</script>
+
+<style scoped>
+
+</style>
