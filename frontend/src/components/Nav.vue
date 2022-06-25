2261f0bd19071fa73cfcf66b7734510e980d7ee2
Nathan Raposo <nathanraposoolivo@gmail.com>
2022-06-25 15:48:49 -0300

    Propriedades computadas com options-api

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+ <router-link to="/">Home</router-link>
+ <router-link to="/about">About</router-link>
+ <router-link to="/users-list">Users List</router-link>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/UsersListView.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2>Lista de Usuários</h2>
+  <ul>
+    <li v-for="user in users" :key="user.id">
+      {{ user.firstName }}
+      <button v-if="user.is_admin">Admin</button>
+      <button v-else>Usuário</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+
+import http from '@/services/http.js';
+
+export default {
+  data() {
+    return {
+      users: [],
+    }
+  },
+
+  async mounted() {
+    try {
+      const {data} = await http.get('api/users')
+      this.users = data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
