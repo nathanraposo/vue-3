d83fff74fcc2a3cb26b97b3c5c3b0cd03f717e3c
Nathan Raposo <nathanraposoolivo@gmail.com>
2022-06-27 22:14:44 -0300

    Props com composition-api

---
+++ frontend/src/components/ProductoInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  Likes: {{ likes }}
+  Comments: {{ comments }}
+</template>
+
+<script setup>
+defineProps({
+  likes: Number,
+  comments: Number
+});
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,16 +1,29 @@
 <template>
   <h2 id="app-products">Products</h2>
   <h2 id="my-app">Products my app</h2>
+
+  <h2>Produto 1</h2>
+  <ProductoInfo
+      :likes=product.likes
+      :comments=product.comments
+  />
+
 </template>
 
-<script>
-export default {
-  name: "Products"
-}
+<script setup>
+
+import ProductoInfo from "@/components/ProductoInfo.vue";
+import {reactive} from "vue";
+
+const product = reactive({
+  likes: 20,
+  comments: 10
+})
+
 </script>
 
 <style>
-#app-products{
+#app-products {
   color: red;
 }
 </style>
