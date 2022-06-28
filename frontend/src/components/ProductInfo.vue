af09178ba2a931b7ec4e817ce1fc689d2e3432fd
Nathan Raposo <nathanraposoolivo@gmail.com>
2022-06-27 22:04:04 -0300

    Props com options-api

---
+++ frontend/src/components/ProductInfo.vue
@@ -0,0 +1,18 @@
+<template>
+  Likes: {{ likes }}
+  Comments: {{ comments }}
+</template>
+
+<script>
+export default {
+  name: "ProductInfo",
+  props: {
+    likes: Number,
+    comments: Number
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/ProductsView.vue
+++ frontend/src/views/ProductsView.vue
@@ -1,11 +1,25 @@
 <template>
   <h2 id="app-products">Products</h2>
   <h2 id="my-app">Products my app</h2>
+
+  <h2>Produto 1</h2>
+  <p>Texto</p>
+  <ProductInfo :likes=product.likes :comments=product.comments />
 </template>
 
 <script>
+import ProductInfo from "@/components/ProductInfo.vue";
 export default {
-  name: "Products"
+  name: "Products",
+  components: {ProductInfo},
+  data(){
+    return{
+      product:{
+        likes:20,
+        comments:30
+      }
+    }
+  }
 }
 </script>
 
